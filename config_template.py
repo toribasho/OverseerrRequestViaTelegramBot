64982f930c0887d1c8fc6350e975b427ece26315
LetsGoDude <paulkrapp94@gmx.de>
2025-02-11 05:12:03 +0100

    Add files via upload

--- config_template.py
+++ config_template.py
@@ -7,7 +7,3 @@ TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN'
 # Access Control Configuration
 # Set a password to protect access. If empty, no access control is applied.
 PASSWORD = ""  # or "" for no access control
-
-# Please use the new whitelist.json in the data folder. 
-# This variable is only included for compatibility with old versions.
-WHITELIST = []  # Telegram User IDs
